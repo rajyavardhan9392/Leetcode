2efb0ba2edc9c549c19a4170faf99b2945dc6904
rajyavardhan9392 <rajyavardhan9392@gmail.com>
2026-09-21 17:49:26 +0530

    Time: 2 ms (58.31%), Space: 42.5 MB (75.24%) - LeetHub

---
+++ 2928-distribute-candies-among-children-i/2928-distribute-candies-among-children-i.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int distributeCandies(int n, int limit) {
+        int c=0;
+        for(int i=0;i<=limit;i++)
+        {
+            for(int j=0;j<=limit;j++)
+            {
+                int k=n-i-j;
+                if(k>=0 && k<=limit)
+                    c++;
+            }
+        }
+        return c;
+    }
+}
